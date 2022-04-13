3cfc8c0768cd5dc421dee255794652b0d9135636
Zett0x <zetto1514@gmail.com>
2022-04-14 01:55:24 +0200

    img loading url added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,3 +6,5 @@ export const DEFAULT_IMG =
   "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo=";
 
 export const LIMIT_RESULTS = 25;
+
+export const IMG_LOADING = "https://i.stack.imgur.com/MEBIB.gif";
