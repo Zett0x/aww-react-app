47a9b93e72de05b2c9749808af84301f51724937
Zett0x <zetto1514@gmail.com>
2022-04-15 01:17:03 +0200

    lastElement removed from store

--- src/reducers/recordsReducer.js
+++ src/reducers/recordsReducer.js
@@ -4,17 +4,10 @@ const initialState = {
   after: null,
   loading: false,
   records: [],
-  lastRecord: null,
 };
 
 export const recordsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.recordsSetLast:
-      return {
-        ...state,
-        lastRecord: state.records[state.records.length - 1],
-      };
-
     case types.recordsSetRecords:
       return {
         ...state,
